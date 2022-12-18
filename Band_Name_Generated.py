44c8a3025037cab05979fb6f3b78831428a92374
Payakan <74350077+MohansGopi@users.noreply.github.com>
2022-12-18 02:30:52 -1200

    Add files via upload

---
+++ Band_Name_Generated.py
@@ -0,0 +1,8 @@
+print("************  Band Name generated system  ***************")
+print("********************  By- payakan  **********************")       
+city_name = input("Name of your city : \n")
+pet_name = input("Name of your pet : \n")
+
+brand_Name = city_name+" "+pet_name
+
+print("The generated name for the band is : "+brand_Name)
